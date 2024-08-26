f7a642ff1759bca42ffa33ad3836877c6d4b5845
Diego Trigo <trigobol23@gmail.com>
2024-08-25 20:21:36 -0400

    Saludo por edad

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
 function saludar() {
     const nombre = prompt("¿Cuál es tu nombre?");
-    const genero = prompt("¿Cuál es tu género? (masculino/femenino)");
+    const genero = prompt("¿Cuál es tu género? (masculino/femenino)").toLowerCase();
+    const edad = parseInt(prompt("¿Cuántos años tienes?"), 10);
+
     let saludo = `Hola, ${nombre}`;
 
-    if (genero.toLowerCase() === "masculino") {
+    if (genero === "masculino") {
         saludo = `Hola, señor ${nombre}`;
-    } else if (genero.toLowerCase() === "femenino") {
+    } else if (genero === "femenino") {
         saludo = `Hola, señora ${nombre}`;
+    }
+
+    if (edad < 18) {
+        saludo = `Hola, joven ${nombre}`;
+    } else {
+        if (genero === "femenino") {
+            saludo += ". ¡Bienvenida!";
+        } else {
+            saludo += ". ¡Bienvenido!";
+        }
     }
 
     document.getElementById("mensaje").textContent = saludo;
